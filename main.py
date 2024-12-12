4423325424da66a53711f36061ef0083f434c04f
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2024-12-12 14:02:03 -0500

    Se realizó el ejercicio 2

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-#Escribir una función que reciba un número entero positivo y devuelva su factorial.
+#Escribir una función que calcule el total de una factura tras aplicarle el IVA.
+#La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar,
+#y devolver el total de la factura. Si se invoca la función sin pasarle el porcentaje de IVA,
+#deberá aplicar un 21%.
 
-number = int(input("Ingrese un número: "))
+def calcular_iva(precio, tasa_iva):
+    iva = precio * (tasa_iva / 100)
+    total_a_pagar = precio + iva
+    return total_a_pagar
 
-import math
+precio = float(input("Ingrese el precio del producto: "))
+aplicar_iva = input("Desea ingresar un IVA diferente (sí/no): ").lower()
 
-print(f"El factorial de {number} es:", math.factorial(number))
+if aplicar_iva == "sí" or aplicar_iva == "si":
+   tasa_iva = float(input("Ingrese la tasa del IVA a aplicar: "))
+else:
+    tasa_iva = 21
+
+
+print(f"El precio del producto sin IVA es: {precio}")
+print(f"EL precio del proucto con IVA es: {calcular_iva(precio, tasa_iva)}")
